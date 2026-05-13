429a726c990ce7f6f67a76796be61b4b28188f4d
jimothymcstubbins <32421811+jimothymcstubbins@users.noreply.github.com>
2026-05-12 20:15:42 -0600

    header href fixes

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,17 +1,31 @@
+// Get base path for dynamic resolution
+function getBasePath() {
+    const isGitHubPages = window.location.hostname.includes('github.io');
+    if (isGitHubPages) {
+        const pathParts = window.location.pathname.split('/').filter(part => part);
+        if (pathParts.length > 0) {
+            return '/' + pathParts[0];
+        }
+    }
+    return '';
+}
+
+const basePath = getBasePath();
+
 // Inject header
 document.getElementById("site-header").innerHTML = `
   <header>
     <nav class="navbar">
       <div class="navbar-brand">
-        <img src="/assets/logo.png" alt="Net Utility Suite" class="navbar-logo">
-        <a href="/index.html" class="logo-text">Net Utility Suite</a>
+        <img src="${basePath}/assets/logo.png" alt="Net Utility Suite" class="navbar-logo">
+        <a href="${basePath}/index.html" class="logo-text">Net Utility Suite</a>
       </div>
 
       <ul class="nav-links">
-        <li><a href="/index.html">Home</a></li>
-        <li><a href="/pages/bulk_url_opener.html">Bulk URL Opener</a></li>
-        <li><a href="/pages/rdap_lookup.html">RDAP Lookup</a></li>
-        <li><a href="/pages/domain_transformer.html">Domain Transformer</a></li>
+        <li><a href="${basePath}/index.html">Home</a></li>
+        <li><a href="${basePath}/pages/bulk_url_opener.html">Bulk URL Opener</a></li>
+        <li><a href="${basePath}/pages/full_rdap_lookup.html">RDAP Lookup</a></li>
+        <li><a href="${basePath}/pages/domain_transformer.html">Domain Transformer</a></li>
       </ul>
     </nav>
   </header>
